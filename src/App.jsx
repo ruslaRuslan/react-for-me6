870c111cb34513aafcd9f967bafa9c28e9dd2321
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-06 16:20:51 +0400

    Routing yaratdix ve App.jsx-de olanlari Routing-e yigdix,

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import { Route, Routes } from "react-router";
-import Calculator from "./components/Calculator";
-import Home from "./components/page/Home";
-import About from "./components/page/About";
-import Users from "./components/Users";
+import Routing from "./routing";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/calculator" element={<Calculator />} />
-        <Route path="/users" element={<Users />} />
-      </Routes>
+      <Routing />
     </>
   );
 }
--- src/page/Home/index.jsx
+++ src/page/Home/index.jsx
@@ -1,4 +1,4 @@
-import PageContainer from "../../PageContainer";
+import PageContainer from "../../components/PageContainer";
 const Home = ({}) => {
   return (
     <PageContainer>
