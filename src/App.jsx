cee1cb5b4f3f62ef0286425d9ec88fd4d88a3cf5
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-06 11:30:57 +0400

    Routes yaradiriq (tam hazir deyil)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-
+import { Routes } from "react-router"
 
 function App() {
 
   return (
     <>
+      <Routes>
+        <Routes path="/" element={<div>this is ana seyfe</div>} />
+        <Routes path="/about" />
+        <Routes path="/calculator" element={<Calculator />} />
 
+
+      </Routes>
     </>
   )
 }
