e3c6e5e5710c442777aa65d2327bd194fc2d287f
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-09 14:27:04 +0400

    localStorage.getItem('mode') setMode-e etdik

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,18 +1,20 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const DarkModeContext = createContext("light");
 
 const DarkModeContextComponent = ({ children }) => {
   const [mode, setMode] = useState("light");
 
-  
+  useEffect(() => {
+    setMode(localStorage.getItem('mode'))
+  }, []);
 
   return (
-    <div className={mode === 'dark' ? 'darkContainer' : ''}>
+    <div className={mode === "dark" ? "darkContainer" : ""}>
       <DarkModeContext.Provider value={[mode, setMode]}>
         {children}
-        </DarkModeContext.Provider>
+      </DarkModeContext.Provider>
     </div>
-  )
-}
+  );
+};
 export default DarkModeContextComponent;
