f91a0d71d3e2ee0e6642b9bcbd571db744d02439
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-09 11:27:30 +0400

    FullNameContext - i elave etdik main.jsx-e

--- src/context/FullNameContext.jsx
+++ src/context/FullNameContext.jsx
@@ -3,6 +3,7 @@ import { createContext } from "react";
 const FN_Context = createContext({ ad: "", email: "" });
 
 const data = {ad:'Ruslan', email:'ruslan@gmail.com'}
+
 const FullNameContext = () => {
     return(
         <FN_Context.Provider value={data}>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import FullNameContext from "./context/FullNameContext.jsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <FullNameContext>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </FullNameContext>
+);
