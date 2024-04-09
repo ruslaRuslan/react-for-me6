4963f5ec9e9f0ed78f7587272ccb122e7acf4d42
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-09 11:14:58 +0400

    Provider etdik FN_Context-i

--- src/context/FullNameContext.jsx
+++ src/context/FullNameContext.jsx
@@ -1,5 +1,11 @@
 import { createContext } from "react";
 
+const FN_Context = createContext({ ad: "", email: "" });
 
-const FN_Context = createContext({ad:'', email: ''})
+const FullNameContext = () => {
+    return(
+        <FN_Context.Provider value={{ad:'Ruslan', email:'ruslan@gmail.com'}}>
 
+        </FN_Context.Provider>
+    )
+};
