f548182a7c5655f948c72f5133be84c53d4d1dd8
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-09 11:54:17 +0400

    Header-e elave etdik ad obyektini p teqine

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,13 @@
 import { NavLink } from "react-router-dom";
 import NavLinkByRuslan from "../NavLinkByRuslan";
+import { useContext } from "react";
+import { FN_Context } from "../../context/FullNameContext";
 
 function Header() {
+  const {ad}= useContext(FN_Context)
   return (
     <nav>
+      <p>xos gelipsen {ad}</p>
       <NavLinkByRuslan title="Home" to="/" />
       <NavLinkByRuslan title="About" to="/about" />
       <NavLinkByRuslan title="calculator" to="/calculator" />
