89e00cbff9e035ece1c8f82d09634c544c687679
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-10 16:11:49 +0400

    Footer

--- src/components/Footer/Index.jsx
+++ src/components/Footer/Index.jsx
@@ -2,16 +2,13 @@ import { useContext } from "react";
 import { FN_Context } from "../../context/FullNameContext";
 
 const Footer = ({}) => {
-
-
-const {email} = useContext(FN_Context)
-
+  const { email } = useContext(FN_Context);
 
   return (
     <div style={{ backgroundColor: "red", margin: "20px 0" }}>
       <p>{email}</p>
-    footer
+      footer
     </div>
-  )
+  );
 };
 export default Footer;
