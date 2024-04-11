1966b55c4e46584df8c56bb1a898258917ce0d32
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-12 00:03:57 +0400

    goturulen numune

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,11 +4,10 @@ import NavLinkByRuslan from "../NavLinkByRuslan";
 function Header() {
   return (
     <nav>
-<NavLinkByRuslan title="Home" to="/" />
-<NavLinkByRuslan title="About" to="/about" />
-<NavLinkByRuslan title="calculator" to="/calculator" />
-<NavLinkByRuslan title="Users" to="/users" />
-
+      <NavLinkByRuslan title="Home" to="/" />
+      <NavLinkByRuslan title="About" to="/about" />
+      <NavLinkByRuslan title="calculator" to="/calculator" />
+      <NavLinkByRuslan title="Users" to="/users" />
     </nav>
   );
 }
